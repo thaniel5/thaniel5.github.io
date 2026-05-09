c8cc08c30d3d5ff21ddf7a4e3d2d5d5d5de5b165
Ethan Campbell <ethangcampbell@gmail.com>
2026-05-09 10:29:27 -0400

    start over

--- convex/schema.ts
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export default defineSchema({
-  comments: defineTable({
-    text: v.string(),
-    username: v.string(),
-  }),
-});
